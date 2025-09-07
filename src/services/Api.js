8179f0561e18911cefac2fb66fe7be8af9aa6e1e
tewodros kassanew <teda16kassa@gmail.com>
2025-09-07 08:30:26 +0300

    update

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,12 +1,14 @@
 export async function getAllproduct() {
-  const response = await fetch("http://localhost:8080/api/product");
+  const response = await fetch("https://your-gebeta.onrender.com/api/product");
   if (!response.ok) throw new Error("products not found");
   const data = await response.json();
   if (data.status === "fail") throw new Error("server error");
   return data.data;
 }
 export async function getProduct(id) {
-  const response = await fetch(`http://localhost:8080/api/product/${id}`);
+  const response = await fetch(
+    `https://your-gebeta.onrender.com/product/${id}`
+  );
   if (!response.ok) throw new Error("products not found");
   const data = await response.json();
   if (data.status === "fail") throw new Error("server error");
