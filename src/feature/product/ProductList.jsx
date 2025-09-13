48d1dde91e81ad8740d734a3082fed849b37af0a
tewodros kassanew <teda16kassa@gmail.com>
2025-09-13 10:42:01 +0300

    update

--- src/feature/product/ProductList.jsx
+++ src/feature/product/ProductList.jsx
@@ -1,19 +1,31 @@
 import { useNavigate, useNavigation } from "react-router-dom";
 import ProductCard from "../../component/ui/ProductCard";
 import Loading from "../../component/ui/Loading";
+import { useEffect } from "react";
 
 function ProductList({ products }) {
   const navigation = useNavigation();
   const navigate = useNavigate();
   const isLoading = navigation.state === "loading";
-  const user = JSON.parse(localStorage.getItem("user"));
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      try {
+        const user = JSON.parse(storedUser);
+        if (user?.Role === "merchant") {
+          navigate("/dashboard");
+        }
+      } catch (err) {
+        console.error("Invalid user JSON", err);
+      }
+    }
+  }, [navigate]);
 
   if (isLoading) {
     return <Loading />;
   }
-  if (user.Role === "merchant") {
-    navigate("/dashboard");
-  }
+
   return (
     <div className="grid grid-cols-4 gap-5">
       {products.map((product) => (
