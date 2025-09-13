466be044b5b3fb2ab7da33a7bb0b2fd13f7a6f51
tewodros kassanew <teda16kassa@gmail.com>
2025-09-13 10:27:45 +0300

    update

--- src/feature/product/ProductList.jsx
+++ src/feature/product/ProductList.jsx
@@ -1,12 +1,18 @@
-import { useNavigation } from "react-router-dom";
+import { useNavigate, useNavigation } from "react-router-dom";
 import ProductCard from "../../component/ui/ProductCard";
 import Loading from "../../component/ui/Loading";
 
 function ProductList({ products }) {
   const navigation = useNavigation();
+  const navigate = useNavigate();
   const isLoading = navigation.state === "loading";
+  const user = JSON.parse(localStorage.getItem("user"));
+
   if (isLoading) {
     return <Loading />;
+  }
+  if (user.Role === "merchant") {
+    navigate("/dashboard");
   }
   return (
     <div className="grid grid-cols-4 gap-5">
